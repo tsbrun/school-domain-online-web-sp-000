240c1e665d9c3b583de45a3dcdee6caf21127dfd
Anya Brun <tsbrun@icloud.com>
2020-07-31 14:33:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+require 'pry'
+
+class School
+  # attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+  end
+
+  def roster
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    # create key with grade that points to array
+    # push student_name into array
+  end
+end
